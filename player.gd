043ef725f3de972dd4234974fb04da6799b9c3a8
greatbrick <speharley@gmail.com>
2024-11-01 23:57:36 -0400

    Added Menu Theme

--- player.gd
+++ player.gd
@@ -7,6 +7,5 @@ var inputMoveDir: Vector2
 func _process(delta):
 	inputMoveDir.y = Input.get_axis("MoveUp", "MoveDown")
 	inputMoveDir.x = Input.get_axis("MoveLeft", "MoveRight")
-	#global_position += (inputMoveDir).normalized() * delta * baseMoveSpeed
 	velocity = (inputMoveDir).normalized() * baseMoveSpeed
 	move_and_slide()
