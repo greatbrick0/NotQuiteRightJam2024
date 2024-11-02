c6cd43ca35b518ab6a0b60281767537b5a30701c
greatbrick <speharley@gmail.com>
2024-11-02 02:17:50 -0400

    Added Shotgun

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
 @export var baseMoveSpeed: float = 170
+@export var currentAmmo: int = 2
+@export var gunActionCooldown: float = 0.4
+@export var gunReloadCooldown: float = 1.4
+var gunActionTime: float = 0
 
+var instanceRef: Node
 @export var bulletScene: PackedScene
 
 var inputMoveDir: Vector2
@@ -20,8 +25,20 @@ func _process(delta):
 	velocity = (inputMoveDir).normalized() * baseMoveSpeed
 	move_and_slide()
 	
-	if(Input.is_action_pressed("Shoot")):
-		Shoot()
+	if(gunActionTime > 0):
+		gunActionTime -= 1.0 * delta
+	else:
+		if(Input.is_action_pressed("Shoot")):
+			if(currentAmmo > 0):
+				Shoot()
+				$Sounds/GunBlastSound.play()
+				currentAmmo -= 1
+				gunActionTime = gunActionCooldown
+			else:
+				currentAmmo = 2
+				gunActionTime = gunReloadCooldown
 
 func Shoot() -> void:
-	pass
+	instanceRef = bulletScene.instantiate()
+	instanceRef.global_position = %BulletPoint.global_position
+	get_parent().add_child(instanceRef)
