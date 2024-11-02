fc6eda475ad712d89ff45c4d8f0f63055610cb6c
greatbrick <speharley@gmail.com>
2024-11-01 23:08:41 -0400

    Added First Sprites

--- player.gd
+++ player.gd
@@ -1,8 +1,12 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var baseMoveSpeed = 100
 
 var inputMoveDir: Vector2
 
 func _process(delta):
 	inputMoveDir.y = Input.get_axis("MoveUp", "MoveDown")
 	inputMoveDir.x = Input.get_axis("MoveLeft", "MoveRight")
-	global_position += (inputMoveDir).normalized() * delta * 30
+	#global_position += (inputMoveDir).normalized() * delta * baseMoveSpeed
+	velocity = (inputMoveDir).normalized() * baseMoveSpeed
+	move_and_slide()
