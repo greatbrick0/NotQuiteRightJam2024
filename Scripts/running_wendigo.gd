f5bbce5ae99d0ef92fa3d932e85f1583b98fecbf
greatbrick <speharley@gmail.com>
2024-11-03 15:12:29 -0500

    Added Running Wendigo Animation

--- Scripts/running_wendigo.gd
+++ Scripts/running_wendigo.gd
@@ -1,5 +1,19 @@
 extends Node2D
 
+@export var v: Vector2 = Vector2.ZERO
+var ran: bool = false
 
 func RunAway() -> void:
+	if(ran): return
+	ran = true
 	$WendigoCry.play()
+	$WendigoSprite.flip_h = true
+	$Hitbox2.queue_free()
+	$TooCloseArea.queue_free()
+	$RunAnim.play("Run")
+
+func NextWave() -> void:
+	get_parent().NewWave()
+
+func _process(delta):
+	global_position += v * delta
